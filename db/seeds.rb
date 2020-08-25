22c14f1e7ed50e99c552c99aed4dd3a527b78bb3
AnansiOmega <ignasbutautas7@gmail.com>
2020-08-25 17:12:22 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do 
+    Student.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name
+    )
+end
+
+
+
+
+
+
+# create_table "students", force: :cascade do |t|
+#     t.string   "first_name", null: false
+#     t.string   "last_name",  null: false
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
