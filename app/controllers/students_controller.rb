bcf3a54ec971d13f27de8b6505f30aa61448a73f
github username <github email address>
2020-08-29 16:19:35 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,13 +5,15 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    @student = Student.find(params[:id])  
   end
 
   def new
   end
 
   def create
+    @students = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to Student.last  #this will redirect you to the last student made.
   end
 
 end
